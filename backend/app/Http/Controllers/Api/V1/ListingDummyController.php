a6c830185a4cc3e5c9272276d3d61ecf7c1fb0b7
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-06 22:27:01 +0800

    new ones

--- backend/app/Http/Controllers/Api/V1/ListingDummyController.php
+++ backend/app/Http/Controllers/Api/V1/ListingDummyController.php
@@ -6,6 +6,15 @@ use App\Http\Controllers\Controller;
 
 class ListingDummyController extends Controller {
     public function update() {
-
+        // $table->string('name');
+        // $table->text('description');
+        // $table->string('province');
+        // $table->string('city');
+        // $table->string('barangay');
+        // $table->string('street');
+        // $table->integer('zip_code');
+        // $table->boolean('approved');
+        // $table->string('price');
+        // $table->integer('maximum_guests');
     }
 }
