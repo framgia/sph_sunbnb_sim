059680f002f5ebab6fd07d993f6a8df87effce7b
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-07 15:08:06 +0800

    started the calendar controller

---
+++ backend/app/Http/Controllers/Api/V1/CalendarController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Client\Request;
+
+class CalendarController extends Controller {
+    public function store(Request $request) {
+
+    }
+}
