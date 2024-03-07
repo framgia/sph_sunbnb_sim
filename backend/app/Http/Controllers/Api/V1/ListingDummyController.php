16219f702eb7b669ec79d682904cfde9dd180a9c
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-07 16:53:59 +0800

    added test for updating the listin

--- backend/app/Http/Controllers/Api/V1/ListingDummyController.php
+++ backend/app/Http/Controllers/Api/V1/ListingDummyController.php
@@ -2,19 +2,61 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Enums\Amenity;
 use App\Http\Controllers\Controller;
+use App\Models\Accommodation;
+use App\Models\Listing;
+use App\Models\Media;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ListingDummyController extends Controller {
-    public function update() {
-        // $table->string('name');
-        // $table->text('description');
-        // $table->string('province');
-        // $table->string('city');
-        // $table->string('barangay');
-        // $table->string('street');
-        // $table->integer('zip_code');
-        // $table->boolean('approved');
-        // $table->string('price');
-        // $table->integer('maximum_guests');
+    public function store(Request $request) {
+        return DB::transaction(function () use ($request) {
+
+            if ($request->has('amenities')) {
+                $inputAmenities = $request->amenities;
+                $validAmenities = Amenity::getConstants();
+
+                foreach ($inputAmenities as $amenity) {
+                    if (! in_array($amenity, $validAmenities)) {
+                        return response()->json(['error' => 'Invalid amenity: '.$amenity], 400);
+                    }
+                }
+            }
+
+            $accommodation = new Accommodation;
+            $accommodation->type = $request->type;
+            $accommodation->bed_count = $request->bed_count;
+            $accommodation->bedroom_count = $request->bedroom_count;
+            $accommodation->bathroom_count = $request->bathroom_count;
+            $accommodation->minimum_days = $request->minimum_days;
+            $accommodation->maximum_days = $request->maximum_days;
+            $accommodation->amenities = json_encode($request->amenities);
+            $accommodation->save();
+
+            $listing = new Listing;
+            $listing->name = $request->name;
+            $listing->description = $request->description;
+            $listing->province = $request->province;
+            $listing->city = $request->city;
+            $listing->barangay = $request->barangay;
+            $listing->street = $request->street;
+            $listing->zip_code = $request->zip_code;
+            $listing->price = $request->price;
+            $listing->maximum_guests = $request->maximum_guests;
+            $listing->listable()->associate($accommodation);
+            $listing->user()->associate(auth()->user());
+            $listing->save();
+
+            foreach ($request->media as $mediaUrl) {
+                $media = new Media;
+                $media->media = json_encode($mediaUrl);
+                $media->listing()->associate($listing);
+                $media->save();
+            }
+
+            return response()->json(['message' => 'Listing created successfully'], 201);
+        });
     }
 }
