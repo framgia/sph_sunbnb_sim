64482047a483d1d73a74056fcd9b96c583ce924d
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-08 16:37:24 +0800

    Backend - set date availability listing

--- backend/database/migrations/2024_03_04_072403_create_calendars_table.php
+++ backend/database/migrations/2024_03_04_072403_create_calendars_table.php
@@ -12,7 +12,6 @@ return new class extends Migration {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
             $table->foreignId('listing_id')->constrained();
-            $table->integer('listing_id');
             $table->date('date');
             $table->boolean('available');
             $table->timestamps();
