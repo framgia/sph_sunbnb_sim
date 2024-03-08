cc321d8e9f0b0feb9ceafe4ccfd5220ca901e9d4
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-08 09:13:47 +0800

    Added new file to update listing

--- backend/app/Http/Controllers/Api/V1/ListingDummyController.php
+++ backend/app/Http/Controllers/Api/V1/ListingDummyController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Enums\Amenity;
 use App\Http\Controllers\Controller;
-use App\Models\Accommodation;
 use App\Models\Listing;
 use App\Models\Media;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
 class ListingDummyController extends Controller {
-    public function store(Request $request) {
-        return DB::transaction(function () use ($request) {
+    public function update(Request $request, $id) {
+        return DB::transaction(function () use ($request, $id) {
+
+            $listing = Listing::findOrFail($id);
+            $accommodation = $listing->listable;
 
             if ($request->has('amenities')) {
                 $inputAmenities = $request->amenities;
@@ -20,43 +23,41 @@ class ListingDummyController extends Controller {
 
                 foreach ($inputAmenities as $amenity) {
                     if (! in_array($amenity, $validAmenities)) {
-                        return response()->json(['error' => 'Invalid amenity: '.$amenity], 400);
+                        return response()->json(['error' => 'Invalid amenity: '.$amenity], Response::HTTP_OK);
                     }
                 }
             }
 
-            $accommodation = new Accommodation;
-            $accommodation->type = $request->type;
-            $accommodation->bed_count = $request->bed_count;
-            $accommodation->bedroom_count = $request->bedroom_count;
-            $accommodation->bathroom_count = $request->bathroom_count;
-            $accommodation->minimum_days = $request->minimum_days;
-            $accommodation->maximum_days = $request->maximum_days;
-            $accommodation->amenities = json_encode($request->amenities);
-            $accommodation->save();
+            $accommodation->update([
+                'type' => $request->type,
+                'bed_count' => $request->bed_count,
+                'bedroom_count' => $request->bedroom_count,
+                'bathroom_count' => $request->bathroom_count,
+                'minimum_days' => $request->minimum_days,
+                'maximum_days' => $request->maximum_days,
+                'amenities' => json_encode($request->amenities),
+            ]);
 
-            $listing = new Listing;
-            $listing->name = $request->name;
-            $listing->description = $request->description;
-            $listing->province = $request->province;
-            $listing->city = $request->city;
-            $listing->barangay = $request->barangay;
-            $listing->street = $request->street;
-            $listing->zip_code = $request->zip_code;
-            $listing->price = $request->price;
-            $listing->maximum_guests = $request->maximum_guests;
-            $listing->listable()->associate($accommodation);
-            $listing->user()->associate(auth()->user());
-            $listing->save();
+            $listing->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'province' => $request->province,
+                'city' => $request->city,
+                'barangay' => $request->barangay,
+                'street' => $request->street,
+                'zip_code' => $request->zip_code,
+                'price' => $request->price,
+                'maximum_guests' => $request->maximum_guests,
+            ]);
 
             foreach ($request->media as $mediaUrl) {
-                $media = new Media;
-                $media->media = json_encode($mediaUrl);
-                $media->listing()->associate($listing);
-                $media->save();
+                Media::updateOrCreate(
+                    ['media' => json_encode($mediaUrl), 'listing_id' => $listing->id],
+                    ['media' => json_encode($mediaUrl)]
+                );
             }
 
-            return response()->json(['message' => 'Listing created successfully'], 201);
+            return response()->json(['message' => 'Listing updated successfully'], Response::HTTP_OK);
         });
     }
 }
