44b516f50bcdc2bc0bb3744f1aaf006c0a8dcd63
Faith Abigail Rosalijos <faithabigail.rosalijos@sun-asterisk.com>
2024-03-11 13:10:33 +0800

    fix theme colors

--- frontend/src/app/components/DividerText.tsx
+++ frontend/src/app/components/DividerText.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 interface DividerTextProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const DividerText: React.FC<DividerTextProps> = ({ children }) => {
-    return (
-        <div className="my-5 flex items-center">
-            <div className="border-primary w-full border" />
-            <span className="w-auto whitespace-nowrap px-2 text-center text-xs text-zinc-500">
-                {children}
-            </span>
-            <div className="border-primary w-full border" />
-        </div>
-    );
+  return (
+    <div className="my-5 flex items-center">
+      <div className="w-full border border" />
+      <span className="w-auto whitespace-nowrap px-2 text-center text-xs text-zinc-500">
+        {children}
+      </span>
+      <div className="w-full border border" />
+    </div>
+  );
 };
 
 export default DividerText;
--- frontend/src/app/components/navbar/NavbarLinks.tsx
+++ frontend/src/app/components/navbar/NavbarLinks.tsx
@@ -15,76 +15,76 @@ import DashboardIcon from "../svgs/Navbar/DashboardIcon";
 import { type NavbarProps } from "@/app/interfaces/NavbarProps";
 
 const NavbarLinks: React.FC<NavbarProps> = (props) => {
-    const pathname = usePathname();
-    const links = {
-        host: [
-            { path: "/", label: "Home", icon: <HomeIcon /> },
-            { path: "/calendar", label: "Calendar", icon: <CalendarIcon /> },
-            { path: "/listings", label: "Listings", icon: <ListingsIcon /> }
-        ],
-        guest: [
-            {
-                path: "/accommodations",
-                label: "Accommodations",
-                icon: <AccommodationsIcon />
-            },
-            {
-                path: "/experiences",
-                label: "Experiences",
-                icon: <ExperiencesIcon />
-            },
-            { path: "/history", label: "History", icon: <HistoryIcon /> }
-        ],
-        admin: [
-            { path: "/dashboard", label: "Dashboard", icon: <DashboardIcon /> },
-            { path: "/approvals", label: "Approvals", icon: <ListingsIcon /> },
-            { path: "/reports", label: "Reports", icon: <ReportsIcon /> },
-            { path: "/users", label: "Users", icon: <UsersIcon /> }
-        ],
-        default: [
-            {
-                path: "/accommodations",
-                label: "Accommodations",
-                icon: <AccommodationsIcon />
-            },
-            {
-                path: "/experiences",
-                label: "Experiences",
-                icon: <ExperiencesIcon />
-            },
-            { path: "/history", label: "History", icon: <HistoryIcon /> }
-        ]
-    };
+  const pathname = usePathname();
+  const links = {
+    host: [
+      { path: "/", label: "Home", icon: <HomeIcon /> },
+      { path: "/calendar", label: "Calendar", icon: <CalendarIcon /> },
+      { path: "/listings", label: "Listings", icon: <ListingsIcon /> }
+    ],
+    guest: [
+      {
+        path: "/accommodations",
+        label: "Accommodations",
+        icon: <AccommodationsIcon />
+      },
+      {
+        path: "/experiences",
+        label: "Experiences",
+        icon: <ExperiencesIcon />
+      },
+      { path: "/history", label: "History", icon: <HistoryIcon /> }
+    ],
+    admin: [
+      { path: "/dashboard", label: "Dashboard", icon: <DashboardIcon /> },
+      { path: "/approvals", label: "Approvals", icon: <ListingsIcon /> },
+      { path: "/reports", label: "Reports", icon: <ReportsIcon /> },
+      { path: "/users", label: "Users", icon: <UsersIcon /> }
+    ],
+    default: [
+      {
+        path: "/accommodations",
+        label: "Accommodations",
+        icon: <AccommodationsIcon />
+      },
+      {
+        path: "/experiences",
+        label: "Experiences",
+        icon: <ExperiencesIcon />
+      },
+      { path: "/history", label: "History", icon: <HistoryIcon /> }
+    ]
+  };
 
-    return (
-        <>
-            {links[props.role].map((link, index) => (
-                <NavbarItem key={index}>
-                    {props.position === NavbarPosition.BOTTOM ? (
-                        <Link
-                            href={link.path}
-                            className={`${pathname === link.path ? "text-primary-600" : ""} flex w-fit flex-col items-center px-3 text-medium text-sm font-semibold`}
-                        >
-                            {link.icon}
-                            <span className="mt-1">
-                                {link.label === "Accommodations" ? "Accomm" : link.label}
-                            </span>
-                        </Link>
-                    ) : (
-                        <Link
-                            href={link.path}
-                            className="flex w-fit flex-col items-center px-4 text-medium text-sm font-semibold"
-                        >
-                            <span>{link.label}</span>
-                            <div
-                                className={`h-1 w-7 rounded-full ${pathname === link.path ? "bg-primary-600" : ""}`}
-                            />
-                        </Link>
-                    )}
-                </NavbarItem>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {links[props.role].map((link, index) => (
+        <NavbarItem key={index}>
+          {props.position === NavbarPosition.BOTTOM ? (
+            <Link
+              href={link.path}
+              className={`${pathname === link.path ? "text-primary" : "text-black"} flex w-fit flex-col items-center px-3 text-medium text-sm font-semibold`}
+            >
+              {link.icon}
+              <span className="mt-1">
+                {link.label === "Accommodations" ? "Accomm" : link.label}
+              </span>
+            </Link>
+          ) : (
+            <Link
+              href={link.path}
+              className="flex w-fit flex-col items-center px-4 text-medium text-sm font-semibold text-black"
+            >
+              <span>{link.label}</span>
+              <div
+                className={`h-1 w-7 rounded-full ${pathname === link.path ? "bg-primary text-primary" : ""}`}
+              />
+            </Link>
+          )}
+        </NavbarItem>
+      ))}
+    </>
+  );
 };
 
 export default NavbarLinks;
