bd0880334fb55508a1c7a3fee139b59834016535
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-12 14:01:55 +0800

    stopped the rerouting

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -4,41 +4,28 @@ import LoginForm from "@/app/(auth)/login/LoginForm";
 import { useDisclosure } from "@nextui-org/react";
 import ResetPasswordModal from "./ResetPasswordModal";
 import LinkSuccessModal from "./LinkSuccessModal";
-import { useRouter } from "next/navigation";
 
 const LoginPage: React.FC = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [isValid, setValid] = useState(false);
-    const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isValid, setValid] = useState(false);
 
-    function onSuccess(): void {
-        onClose();
-        router.replace("reset-password");
-    }
-
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between">
-            <LoginForm onResetPress={onOpen} />
-            {!isValid ? (
-                <ResetPasswordModal
-                    size="sm"
-                    isOpen={isOpen}
-                    onClose={onClose}
-                    onSubmit={() => {
-                        setValid(true);
-                    }}
-                />
-            ) : (
-                <LinkSuccessModal
-                    size="sm"
-                    isOpen={isOpen}
-                    onClose={() => {
-                        onSuccess();
-                    }}
-                />
-            )}
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <LoginForm onResetPress={onOpen} />
+      {!isValid ? (
+        <ResetPasswordModal
+          size="sm"
+          isOpen={isOpen}
+          onClose={onClose}
+          onSubmit={() => {
+            setValid(true);
+          }}
+        />
+      ) : (
+        <LinkSuccessModal size="sm" isOpen={isOpen} onClose={onClose} />
+      )}
+    </main>
+  );
 };
 
 export default LoginPage;
--- frontend/src/app/(auth)/reset-password/page.tsx
+++ frontend/src/app/(auth)/reset-password/page.tsx
@@ -7,43 +7,42 @@ import { useDisclosure } from "@nextui-org/react";
 import ResetSuccessModal from "./ResetSuccessModal";
 
 const ResetPasswordPage: React.FC = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
 
-    function HandleSubmit(event: React.FormEvent): void {
-        event.preventDefault();
-        onOpen();
-    }
-    function HandleLogin(): void {
-        onClose();
-        router.replace("login");
-    }
+  function HandleSubmit(event: React.FormEvent): void {
+    event.preventDefault();
+    onOpen();
+  }
+  function HandleLogin(): void {
+    onClose();
+    router.replace("login");
+  }
 
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-start px-24 py-10">
-            <LogoLargeIcon />
-            <div className="w-2/5">
-                <div className="my-5 flex flex-col">
-                    <span className="text-center text-xl font-bold">
-                        Reset your password
-                    </span>
-                    <span className="w-4/5 self-center text-center text-zinc-500">
-                        Enter a new password below to gain access to your
-                        account.
-                    </span>
-                </div>
-                <div className="w-full">
-                    <ResetPasswordForm handleSubmit={HandleSubmit} />
-                </div>
-            </div>
-            <ResetSuccessModal
-                size="sm"
-                isOpen={isOpen}
-                onClose={HandleLogin}
-                onPress={HandleLogin}
-            />
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start px-24 py-10">
+      <LogoLargeIcon />
+      <div className="w-2/5">
+        <div className="my-5 flex flex-col">
+          <span className="text-center text-xl font-bold">
+            Reset your password
+          </span>
+          <span className="w-4/5 self-center text-center text-zinc-500">
+            Enter a new password below to gain access to your account.
+          </span>
+        </div>
+        <div className="w-full">
+          <ResetPasswordForm handleSubmit={HandleSubmit} />
+        </div>
+      </div>
+      <ResetSuccessModal
+        size="sm"
+        isOpen={isOpen}
+        onClose={HandleLogin}
+        onPress={HandleLogin}
+      />
+    </main>
+  );
 };
 
 export default ResetPasswordPage;
