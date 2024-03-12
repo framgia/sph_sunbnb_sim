f0fa871d2fbe7cd9f86e350a9c9f19f5584ac964
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-12 14:05:23 +0800

    merged stash

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -4,10 +4,28 @@ import LoginForm from "@/app/(auth)/login/LoginForm";
 import { useDisclosure } from "@nextui-org/react";
 import ResetPasswordModal from "./ResetPasswordModal";
 import LinkSuccessModal from "./LinkSuccessModal";
+import { useRouter } from "next/navigation";
+import { forgetPassword } from "@/app/utils/helpers/passwordHelper";
 
 const LoginPage: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isValid, setValid] = useState(false);
+
+  const [email, setEmail] = useState("");
+
+  const router = useRouter();
+
+  function onSuccess(): void {
+    onClose();
+    router.replace("reset-password");
+  }
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
+    e.preventDefault();
+    console.log("Submitting");
+    const result = await forgetPassword(email);
+    setValid(result);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -17,12 +35,18 @@ const LoginPage: React.FC = () => {
           size="sm"
           isOpen={isOpen}
           onClose={onClose}
-          onSubmit={() => {
-            setValid(true);
-          }}
+          onSubmit={onSubmit}
+          email={email}
+          setEmail={setEmail}
         />
       ) : (
-        <LinkSuccessModal size="sm" isOpen={isOpen} onClose={onClose} />
+        <LinkSuccessModal
+          size="sm"
+          isOpen={isOpen}
+          onClose={() => {
+            onSuccess();
+          }}
+        />
       )}
     </main>
   );
--- frontend/src/app/config/config.ts
+++ frontend/src/app/config/config.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface Config {
-    backendUrl: string;
+  backendUrl: string;
 }
 
 const config: Config = {
-    backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8000"
+  backendUrl:
+    process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8000/api/v1"
 };
 
 export default config;
