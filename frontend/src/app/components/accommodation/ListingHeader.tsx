cb9a8e384f9fc99f42cf5b76fb956d75f9862f10
John Vergil Aparri <156734177+johnvergilaparriSA@users.noreply.github.com>
2024-03-13 13:09:00 +0800

    Integrate signup, login, logout, and middleware (#32)
    * Added User account management page and components
    
    * Accommodation details components and page
    
    * ignore local vscode
    
    * Change avatar to be based on host name initials
    
    * changed let to const to fix lint error
    
    * create user helper, added useFormState in signup page, and added names for inputs
    
    * moved signup handling to another component
    
    * moved types to one file and storing to cookies
    
    * Authentication and redirection w/o google
    
    * sign up redirection and navbar is based on cookies
    
    * fixed imports
    
    * added logout
    
    * updated login
    
    * Fixed logout and improved navbar responsiveness
    
    * Only store jwt (get user from subject value) and new dependency
    
    * added middleware
    
    * Removed .rnd
    
    ---------
    
    Co-authored-by: rianfrancis <rianfrancis.cabanero@sun-asterisk.com>

--- frontend/src/app/components/accommodation/ListingHeader.tsx
+++ frontend/src/app/components/accommodation/ListingHeader.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React from "react";
 import { Avatar, Image } from "@nextui-org/react";
-import type { MediaType } from "@/app/interfaces/MediaType";
+import type { MediaType } from "@/app/interfaces/types";
+import { getInitials } from "@/app/utils/helpers/getInitials";
 
 interface ListingHeaderProps {
   accomodationName: string;
@@ -35,15 +36,6 @@ const ListingHeader: React.FC<ListingHeaderProps> = ({
   modifiedAt,
   images
 }) => {
-  function getInitials(name: string): string {
-    const names = name.split(" ");
-    let initials = names[0].substring(0, 1).toUpperCase();
-
-    if (names.length > 1) {
-      initials += names[names.length - 1].substring(0, 1).toUpperCase();
-    }
-    return initials;
-  }
   return (
     <div>
       <div className="mb-2">
