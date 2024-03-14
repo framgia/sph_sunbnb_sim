6fd292fa18a6556c9c286d64af2c38696edf96ce
Faith Abigail Rosalijos <faithabigail.rosalijos@sun-asterisk.com>
2024-03-14 13:56:43 +0800

    move business logic to model

--- backend/app/Http/Controllers/Api/V1/AuthController.php
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -2,29 +2,18 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Enums\UserStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\LoginRequest;
 use App\Http\Requests\V1\RegistrationRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller {
     public function login(LoginRequest $request) {
         $request->validated();
-
-        if (! Auth::attempt($request->only('email', 'password'))) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Invalid credentials',
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-
-        $user = User::find(Auth::user()->id);
-        $userToken = $user->createToken('appToken');
+        $user = User::authenticateUser($request->all());
+        $userToken = $user->createToken('Personal Access Token');
 
         return response()->json([
             'success' => true,
@@ -36,17 +25,8 @@ class AuthController extends Controller {
 
     public function register(RegistrationRequest $request) {
         $request->validated();
-
-        $user = User::create([
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'role' => $request->role,
-            'status' => UserStatus::ACTIVE,
-            'email' => $request->email,
-            'password' => $request->password,
-        ]);
-
-        $userToken = $user->createToken('appToken');
+        $user = User::instantiateUser($request->all());
+        $userToken = $user->createToken('Personal Access Token');
 
         return response()->json([
             'success' => true,
@@ -56,8 +36,8 @@ class AuthController extends Controller {
         ], Response::HTTP_CREATED);
     }
 
-    public function logout(Request $request) {
-        $request->user()->token()->revoke();
+    public function logout() {
+        request()->user()->token()->revoke();
 
         return response()->json([
             'success' => true,
