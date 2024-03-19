50b908116c4e62767082facfe6db4ca128dbcab4
brettjosef <brettjosef.galvez@sun-asterisk.com>
2024-03-19 18:11:12 +0800

    check user role middleware initialized

--- backend/app/Http/Middleware/CheckUserRole.php
+++ backend/app/Http/Middleware/CheckUserRole.php
@@ -6,15 +6,17 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckUserRole
-{
+class CheckUserRole {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
+    public function handle(Request $request, Closure $next, $role): Response {
+        if ($request->user() && $request->user()->role === $role) {
+            return $next($request);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
     }
 }
