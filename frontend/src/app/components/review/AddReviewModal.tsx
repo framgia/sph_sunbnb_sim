7e5045346de684bf102add9919fc33758e26bd89
johnvergilaparriSA <johnvergil.aparri@sun-asterisk.com>
2024-03-20 15:06:46 +0800

    Change div svg to iconOnly button

--- frontend/src/app/components/review/AddReviewModal.tsx
+++ frontend/src/app/components/review/AddReviewModal.tsx
@@ -1,5 +1,5 @@
 import type { ModalProps } from "@/app/interfaces/ModalProps";
-import { Modal, ModalContent } from "@nextui-org/react";
+import { Button, Modal, ModalContent } from "@nextui-org/react";
 import React from "react";
 import ChevronLeftIcon from "../svgs/Calendar/ChevronLeftIcon";
 import AddReviewForm from "./AddReviewForm";
@@ -19,14 +19,11 @@ const ReviewModal: React.FC<ModalProps> = ({ isOpen, onClose, size }) => {
             <div className="p-5">
               <div className="mb-2 flex flex-col">
                 <div className="flex flex-row">
-                  <div
-                    className="my-1 cursor-pointer self-start"
-                    onClick={onClose}
-                  >
+                  <Button className="m-0 bg-white" isIconOnly onPress={onClose}>
                     <ChevronLeftIcon />
-                  </div>
+                  </Button>
                   <div className="flex flex-col">
-                    <span className="mb-1 items-center text-2xl font-bold">
+                    <span className="my-1 items-center text-2xl font-bold">
                       Rate your stay!
                     </span>
                     <span className="text-md ">
