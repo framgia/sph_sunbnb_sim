cda464b83a17df66e46df57bab848c377082909a
johnvergilaparriSA <johnvergil.aparri@sun-asterisk.com>
2024-03-20 14:27:06 +0800

    removed test components in index page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-import React, { useState } from "react";
-import ReviewModal from "./components/review/AddReviewModal";
-import { Button } from "@nextui-org/react";
+import React from "react";
 
 const Home: React.FC = () => {
-  const [isOpen, setOpen] = useState(false);
   return (
     <>
       <h1>Home Page</h1>
-      <Button
-        onPress={() => {
-          setOpen(true);
-        }}
-      >
-        Open me!
-      </Button>
-      <ReviewModal
-        isOpen={isOpen}
-        onClose={() => {
-          setOpen(false);
-        }}
-        size="xl"
-      />
     </>
   );
 };
