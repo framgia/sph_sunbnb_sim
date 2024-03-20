ae39cc19564748baafa1b0eaa56b9167777f5bb4
John Vergil Aparri <156734177+johnvergilaparriSA@users.noreply.github.com>
2024-03-20 09:51:09 +0800

    Added components for displaying reviews in listing details page. (#50)
    * route.ts types modified and NavBar dropdown Links removed
    
    * Added components for displaying reviews
    
    * Modified amenity header
    
    * padding changes to align avatar
    
    * Added show all amenities functionality

--- frontend/src/app/components/navbar/NavbarBottom.tsx
+++ frontend/src/app/components/navbar/NavbarBottom.tsx
@@ -1,17 +1,24 @@
 import { Navbar, NavbarContent } from "@nextui-org/react";
 import React from "react";
 import NavbarLinks from "./NavbarLinks";
-import { NavbarPosition, UserRole } from "@/app/utils/enums";
+import { NavbarPosition } from "@/app/utils/enums";
+import { checkCookies } from "@/app/utils/helpers/userHelper";
+import { redirect } from "next/navigation";
 
-const NavbarBottom: React.FC = () => {
+const NavbarBottom: React.FC = async () => {
+  const user = await checkCookies();
+
   return (
     <Navbar
       className="border-top sticky bottom-0 border sm:!hidden"
       isBlurred={false}
     >
       <NavbarContent justify="center" className="w-full">
-        {/* TODO: Fetch user role from logged in user */}
-        <NavbarLinks role={UserRole.HOST} position={NavbarPosition.BOTTOM} />
+        {user !== undefined && user !== null ? (
+          <NavbarLinks role={user.role} position={NavbarPosition.BOTTOM} />
+        ) : (
+          redirect("/login")
+        )}
       </NavbarContent>
     </Navbar>
   );
