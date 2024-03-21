58534a9a3ba74db4432f1aef3239cb3b59b8cb97
johnvergilaparriSA <johnvergil.aparri@sun-asterisk.com>
2024-03-21 10:00:41 +0800

    Updated add review

--- frontend/src/app/components/review/AddReviewModal.tsx
+++ frontend/src/app/components/review/AddReviewModal.tsx
@@ -1,9 +1,5 @@
 import type { ModalProps } from "@/app/interfaces/ModalProps";
-<<<<<<< HEAD
-import { Modal, ModalContent } from "@nextui-org/react";
-=======
 import { Button, Modal, ModalContent } from "@nextui-org/react";
->>>>>>> c293b57d477ab4b63f6a6a28174828108f6f916f
 import React from "react";
 import ChevronLeftIcon from "../svgs/Calendar/ChevronLeftIcon";
 import AddReviewForm from "./AddReviewForm";
@@ -23,22 +19,11 @@ const ReviewModal: React.FC<ModalProps> = ({ isOpen, onClose, size }) => {
             <div className="p-5">
               <div className="mb-2 flex flex-col">
                 <div className="flex flex-row">
-<<<<<<< HEAD
-                  <div
-                    className="my-1 cursor-pointer self-start"
-                    onClick={onClose}
-                  >
-                    <ChevronLeftIcon />
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="mb-1 items-center text-2xl font-bold">
-=======
                   <Button className="m-0 bg-white" isIconOnly onPress={onClose}>
                     <ChevronLeftIcon />
                   </Button>
                   <div className="flex flex-col">
                     <span className="my-1 items-center text-2xl font-bold">
->>>>>>> c293b57d477ab4b63f6a6a28174828108f6f916f
                       Rate your stay!
                     </span>
                     <span className="text-md ">
