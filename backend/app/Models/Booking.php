9baf37dd585495264e3e10cc0fc1cfa10b12291c
caraencabo <caracarmel.encabo@sun-asterisk.com>
2024-03-22 11:38:12 +0800

    add manage bookings functionalities

--- backend/app/Models/Booking.php
+++ backend/app/Models/Booking.php
@@ -8,10 +8,14 @@ use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class Booking extends Model {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'start_date', 'end_date', 'number_of_guests', 'total_price', 'status',
@@ -23,6 +27,10 @@ class Booking extends Model {
 
     public function listing(): BelongsTo {
         return $this->belongsTo(Listing::class);
+    }
+
+    public function calendars(): HasMany {
+        return $this->hasMany(Calendar::class);
     }
 
     public static function createBooking($request): self {
@@ -68,5 +76,61 @@ class Booking extends Model {
         }
 
         return $calendarDates;
+    }
+
+    public function deleteBooking() {
+        if (in_array($this->status, [BookingStatus::CANCELLED, BookingStatus::REFUSED, BookingStatus::DONE])) {
+            $this->deleteCalendarEntries();
+            $this->delete();
+        } elseif ($this->status === BookingStatus::PENDING) {
+            abort(400, 'Cancel your booking first.');
+        } elseif ($this->status === BookingStatus::UPCOMING) {
+            abort(400, "You can't delete this booking.");
+        }
+    }
+
+    private function deleteCalendarEntries() {
+        foreach ($this->calendars as $calendarEntry) {
+            $calendarEntry->delete();
+        }
+    }
+
+    public function cancelBooking(): void {
+        if (in_array($this->status, [BookingStatus::PENDING, BookingStatus::UPCOMING])) {
+            $this->update(['status' => BookingStatus::CANCELLED]);
+            $this->updateCalendarEntries(true);
+        } else {
+            abort(400, "You can't cancel this booking.");
+        }
+    }
+
+    private function updateCalendarEntries(bool $available): void {
+        foreach ($this->calendars as $calendarEntry) {
+            $calendarEntry->update(['available' => $available]);
+        }
+    }
+
+    public static function paginateBookingsByUser($userId, Request $request) {
+        $perPage = $request->query('per_page', 5);
+
+        return static::where('user_id', $userId)
+            ->with(['listing', 'user'])
+            ->paginate($perPage);
+    }
+
+    public static function bookingsResponse($bookings) {
+        return [
+            'success' => true,
+            'bookings' => $bookings->items(),
+            'pagination' => [
+                'current_page' => $bookings->currentPage(),
+                'per_page' => $bookings->perPage(),
+                'total' => $bookings->total(),
+                'next_page_url' => $bookings->nextPageUrl(),
+                'path' => $bookings->path(),
+                'prev_page_url' => $bookings->previousPageUrl(),
+                'to' => $bookings->lastItem(),
+            ],
+        ];
     }
 }
