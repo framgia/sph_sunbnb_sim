145f4d68f1160f123f2827c1e9e765540cea9fda
johnvergilaparriSA <johnvergil.aparri@sun-asterisk.com>
2024-04-11 11:17:58 +0800

    popular listings section added

--- frontend/src/app/(admin)/dashboard/page.tsx
+++ frontend/src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,70 @@
 import React from "react";
 import AdminAnalytics from "./AdminAnalytics";
+import { Accommodation, Listing } from "@/app/interfaces/types";
+import { ListingStatus, UserRole } from "@/app/utils/enums";
+import PopularListingCard from "@/app/components/admin/PopularListingCard";
+import PopularListings from "@/app/components/admin/PopularListingsSection";
 
 const DashboardPage: React.FC = () => {
+  const dummyListing: Listing = {
+    id: 13,
+    user_id: 1,
+    status: ListingStatus.PENDING,
+    name: "Farmmmm",
+    description: "Description of the listing",
+    province: "Example Province",
+    city: "Example City",
+    barangay: "Example Barangay",
+    street: "Example Street",
+    zip_code: 12345,
+    price: 100.0,
+    maximum_guests: 5,
+    listable_type: "App\\Models\\Accommodation",
+    listable_id: 15,
+    created_at: new Date("2024-03-08T08:48:24.000000Z"),
+    updated_at: new Date("2024-03-08T08:48:24.000000Z"),
+    deleted_at: null,
+    media: [
+      {
+        id: 31,
+        listing_id: 13,
+        media:
+          '["https://utfs.io/f/708298d7-aad9-491e-85c3-82f9cd0a79e3-g47goy.jpg"]',
+        created_at: "2024-03-08T08:48:24.000000Z",
+        updated_at: "2024-03-08T08:48:24.000000Z",
+        deleted_at: null
+      },
+      {
+        id: 32,
+        listing_id: 13,
+        media:
+          '["https://utfs.io/f/ca88b394-c700-4cae-8176-6fe8d8b838b1-5nw4vb.png"]',
+        created_at: "2024-03-08T08:48:24.000000Z",
+        updated_at: "2024-03-08T08:48:24.000000Z",
+        deleted_at: null
+      },
+      {
+        id: 33,
+        listing_id: 13,
+        media:
+          '["https://utfs.io/f/f5532a6f-e721-4458-9ffe-2ced484b7188-2487m.jpg"]',
+        created_at: "2024-03-08T08:48:24.000000Z",
+        updated_at: "2024-03-08T08:48:24.000000Z",
+        deleted_at: null
+      }
+    ],
+    user: {
+      id: 1,
+      first_name: "Aracs",
+      last_name: "Encabo",
+      email: "janedoe@example.com",
+      created_at: "2024-03-08T07:08:20.000000Z",
+      role: UserRole.HOST,
+      status: "active",
+      updated_at: ""
+    }
+  };
+  const popularListings = [dummyListing, dummyListing, dummyListing];
   return (
     <>
       <div>
@@ -19,11 +82,7 @@ const DashboardPage: React.FC = () => {
       />
       <div className="mt-10">
         <span className="text-xl font-bold">Popular Listings</span>
-        <div className="my-5 flex h-56 w-full flex-row">
-          <div className="mr-2 flex w-1/3 bg-primary"></div>
-          <div className="mr-2 flex w-1/3 bg-primary"></div>
-          <div className="flex w-1/3 bg-primary"></div>
-        </div>
+        <PopularListings listings={popularListings} />
       </div>
       <div>
         <span className="text-xl font-bold">User Traffic</span>
