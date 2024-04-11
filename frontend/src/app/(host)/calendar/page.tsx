7f4212e10b41b49e238a4409778fa2964e6d6fc7
faith-rosalijos <156732790+faith-rosalijos@users.noreply.github.com>
2024-04-11 17:19:33 +0800

    make calendar page mobile responsive (#109)
    * make calendar page mobile responsive
    
    * make content center

--- frontend/src/app/(host)/calendar/page.tsx
+++ frontend/src/app/(host)/calendar/page.tsx
@@ -6,7 +6,7 @@ const CalendarPage: React.FC = async () => {
   const listings = await getListingNames();
 
   return (
-    <main className="overflow-y-auto md:fixed md:end-0 md:mt-[-20px] md:flex md:h-full md:w-screen">
+    <main className="md:fixed md:end-0 md:mt-[-20px] md:flex md:h-full md:w-screen md:overflow-y-auto">
       {listings === undefined || listings?.length === 0 ? (
         <p className="mx-auto mt-5 text-center text-gray-500">
           No listings available.
