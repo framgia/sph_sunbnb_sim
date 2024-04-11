5add0811da2a6edf8c165f40721d84a859d261c6
John Vergil Aparri <156734177+johnvergilaparriSA@users.noreply.github.com>
2024-04-11 16:40:10 +0800

    Admin Analytics markup (#108)
    * initial markup
    
    * added dummy numbers
    
    * Analytics initial almost done
    
    * added temporary divs
    
    * popular listings section added
    
    * added charts
    
    * Finished Charts
    
    * cleaned lint errors
    
    * lint clean

---
+++ frontend/src/app/components/admin/PopularListingsSection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PopularListingCard from "./PopularListingCard";
+import type { Listing } from "@/app/interfaces/types";
+
+const PopularListings: React.FC<{
+  listings: Listing[];
+}> = ({ listings }) => {
+  const top3 = listings.slice(0, 3);
+
+  return (
+    <div className="my-5 flex h-56 w-full flex-row">
+      <div className="mr-2 w-1/3">
+        <PopularListingCard listing={top3[0]} />
+      </div>
+      <div className="mr-2 w-1/3">
+        <PopularListingCard listing={top3[1]} />
+      </div>
+      <div className="w-1/3">
+        <PopularListingCard listing={top3[2]} />
+      </div>
+    </div>
+  );
+};
+
+export default PopularListings;
